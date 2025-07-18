9d433f9eece19488a17b504582c66ce3b1e0a55e
Subed <subedshahxi@gmail.com>
2025-07-18 15:43:09 +0545

    feat: route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
-  return <h2>HELLO</h2>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
