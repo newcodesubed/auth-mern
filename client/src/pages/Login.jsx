2e7f928602ce3728b36e4dcbff28e6022d04cf66
Subed <subedshahxi@gmail.com>
2025-07-18 16:07:55 +0545

    feat: added login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+
 function Login() {
-  return <div>login</div>;
+  const loginUser = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={loginUser}>
+        <label>Email</label>
+        <input type="text" placeholder="eg: john@gmail.com ..." />
+        <label>Password</label>
+        <input type="text" placeholder="eg: s@9***** ..." />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
