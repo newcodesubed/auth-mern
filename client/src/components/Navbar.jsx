b0c140f0422da65e6d295f70ead3790d147c5b27
Subed <subedshahxi@gmail.com>
2025-07-18 16:00:09 +0545

    feat: added Navbar Link and register page form

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+    </>
+  );
 }
 
 export default Navbar;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,22 @@
+import React from "react";
+
 function Register() {
-  return <div>Register</div>;
+  const registerUser = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input type="text" placeholder="eg: john doe ..." />
+        <label>Email</label>
+        <input type="text" placeholder="eg: john@gmail.com ..." />
+        <label>Password</label>
+        <input type="text" placeholder="eg: s@9***** ..." />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
