527a9cf7ace8049df11c8f5e71a5e9a3f4a7c919
Subed <subedshahxi@gmail.com>
2025-07-22 23:59:20 +0545

    feat: created func for comparePassword

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -16,3 +16,12 @@ const hashPassword = (password) => {
     });
   });
 };
+
+const comparePassword = (password, hashed) => {
+  return bcrypt.compare(password, hashed);
+};
+
+module.exports = {
+  hashPassword,
+  comparePassword,
+};
