89beaa34ca8015d49c8dcfe36677412bbd2ed7e4
Subed <subedshahxi@gmail.com>
2025-07-22 23:55:17 +0545

    feat: implementing hashing password with hash

---
+++ server/helpers/auth.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt");
+const { reject } = require("bcrypt/promises");
+
+const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
