909ecf8ae7c4c33daaf6e17b404801ea7af45f9f
Subed <subedshahxi@gmail.com>
2025-07-23 15:12:00 +0545

    middleware added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const { mongoose } = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 //database connection
@@ -12,6 +14,8 @@ mongoose
 
 // middleware
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/authRoutes"));
 
